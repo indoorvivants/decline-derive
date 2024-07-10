be0d7711e602e6407fcbc6c8c77f163004979076
Anton Sviridov <keynmol@gmail.com>
2024-07-10 16:03:24 +0200

    WIP

--- test.scala
+++ test.scala
@@ -13,8 +13,19 @@ enum MyCLI derives CommandApplication:
   case Search(location: String, repl: Boolean)
   case Test(bla: Int)
 
+case class Stuff(flag: Int, location: Option[String]) derives CommandApplication
+
 @main def hello =
   println(
     summon[CommandApplication[MyCLI]].opt
-      .parse(Seq("index", "--language", "java"))
+      .parse(
+        Seq("index", "--index-location", "index.trig", "--language", "scala")
+      )
+  )
+
+  println(
+    summon[CommandApplication[Stuff]].opt
+      .parse(
+        Seq("--help")
+      )
   )
