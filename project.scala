f80b79274abdebf2b9a811204ae3e1a5812d431f
Anton Sviridov <keynmol@gmail.com>
2024-07-19 12:00:23 +0100

    Correct repo link

--- project.scala
+++ project.scala
@@ -10,8 +10,8 @@
 //> using publish.secretKeyPassword env:PGP_PASSPHRASE
 //> using publish.license Apache-2.0
 //> using publish.developer "velvetbaldmime|Anton Sviridov|https://indoorvivants.com"
-//> using publish.vcs github:indoorivants/decline-derive
-//> using publish.url https://github.com/indoorivants/decline-derive
+//> using publish.vcs github:indoorvivants/decline-derive
+//> using publish.url https://github.com/indoorvivants/decline-derive
 //> using scala 3.3.3
 //> using nativeVersion 0.4.17
 //> using jsVersion 1.16.0
